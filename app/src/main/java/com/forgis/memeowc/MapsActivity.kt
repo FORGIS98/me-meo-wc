9965511e3533f2a8e0872109cba54451bc286e64
FORGIS98 <jorgesolgonzalez1998@gmail.com>
2022-01-30 20:08:55 +0100

    Initial Commit + Places lib

---
+++ app/src/main/java/com/forgis/memeowc/MapsActivity.kt
@@ -0,0 +1,41 @@
+package com.forgis.memeowc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+import com.forgis.memeowc.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(40.4168219, -3.7040036)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Madrid"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+}
